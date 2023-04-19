ffcb60d8fd6d95229b8e20b223af923927e5f248
LAPTOP-7RHK8L29\toshi <toshiya_kutsukake@hotmail.com>
2023-04-19 20:33:09 +0200

    commit - push test

---
+++ shrodinger-test.py
@@ -0,0 +1,26 @@
+# pip install sympy
+# test test test test test
+
+import math
+import sympy
+
+
+sympy.init_printing()
+
+sympy.var("qi qj")
+
+
+S = (math.pi/(qi+qj))**(3/2)
+H = 0.5*6*qi*qj/(qi+qj)*S - 2*math.pi/(qi+qj)
+
+
+s = sympy.simplify(S)
+H = sympy.simplify(H)
+
+
+print(S)
+print(H)
+
+
+print(sympy.latex(S))
+print(sympy.latex(H))
